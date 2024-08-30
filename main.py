2a204ab529aebc341b3778ec3dee4f8eade571ce
ducdn <ducdo23022001@gmail.com>
2024-08-30 23:24:10 +0700

    add submodule

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from test_module1.math.caculate import sum, sub, mul
 
 
 app = FastAPI()
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/add/{a}/{b}")
+def read_item(a: int, b: int):
+    return {"result": sum(a, b)}
+
+@app.get("/sub/{a}/{b}")
+def read_item(a: int, b: int):
+    return {"result": sub(a, b)}
+
+@app.get("/mul/{a}/{b}")
+def read_item(a: int, b: int):
+    return {"result": mul(a, b)}
